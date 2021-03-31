f0e35b2235d7ce4382d300e65e14cbd61456089b
Domas Sausanavicius <domas.sausanavicius@gmail.com>
2021-03-31 21:38:08 +0300

    Switch testing

--- LearningApp/Lesson3/Program3.cs
+++ LearningApp/Lesson3/Program3.cs
@@ -6,7 +6,28 @@ namespace LearningApp.Lesson3
     {
         static void Main()
         {
-            Console.WriteLine("HI");
+            Console.WriteLine("Iveskite pirma skaiciu:");
+            int skaicius1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite antra skaiciu:");
+            int skaicius2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite trecia skaiciu:");
+            int skaicius3 = Convert.ToInt32(Console.ReadLine());
+
+            switch (skaicius1)
+            {
+                case 1:
+                    Console.WriteLine(skaicius1 + skaicius2 + skaicius3);
+                    break;
+                case 2:
+                    Console.WriteLine(skaicius1 - skaicius3);
+                    break;
+                case 3:
+                    Console.WriteLine(skaicius2 * skaicius3);
+                    break;
+                default:
+                    Console.WriteLine("Ivestas kitas skaicius");
+                    break;
+            }
         }
     }
 }
