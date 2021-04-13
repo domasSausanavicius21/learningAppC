5b1e7feebcbbadc7d615c20fe913e423f19d62c4
Domas Sausanavicius <domas.sausanavicius@gmail.com>
2021-04-13 22:41:00 +0300

    BigHomeWork Start
    Pirma ir antra uzduotis

--- LearningApp/BigHomeWork1/BigHomeWork1.cs
+++ LearningApp/BigHomeWork1/BigHomeWork1.cs
@@ -10,19 +10,48 @@ namespace LearningApp.BigHomeWork1
     {
         static void Main()
         {
+
             Console.WriteLine("Iveskite skaiciu [-9...9]:");
-            string number1 = Console.ReadLine();
-            stringIsInt();
+            string input1 = Console.ReadLine();
+            checkString(input1);
         }
 
-        static void stringIsInt()
+        static bool checkString(string input1)
         {
-            string zodis = "Labas";
-            for (int i = 0; i < zodis.Length; i++)
+
+            char[] skaiciai = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+
+            bool isInteger = false;
+
+            for (int i = 0; i < input1.Length; i++)
             {
-                char simbolis = zodis[i];
-                Console.WriteLine(simbolis);
+                char simbolis = input1[i];
+                //Console.WriteLine(simbolis);
+
+                for (int j = 0; j < skaiciai.Length; j++)
+                {
+                    char simbolis1 = skaiciai[i];
+
+                    if (simbolis == simbolis1)
+                    {
+                        isInteger = true;
+                    }
+                    else
+                    {
+                        isInteger = false;
+                    }
+                }
             }
+            if (isInteger)
+            {
+                Console.WriteLine("Ivestas tekstas yra skaicius");
+            }
+            else
+            {
+                Console.WriteLine("Ivestas tekstas nera skaicius");
+            }
+
+            return isInteger;
         }
     }
 }
